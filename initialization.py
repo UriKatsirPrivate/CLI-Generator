f7db4301f13729a6d462d1706a88cc03bc2b68d2
Uri Katsir <46541339+UriKatsirPrivate@users.noreply.github.com>
2023-09-10 01:06:42 +0300

    code cleanup

--- initialization.py
+++ initialization.py
@@ -1,22 +1,12 @@
-import os
-import json
-import vertexai
 from langchain.llms import VertexAI
-# from google.cloud import secretmanager
 
+# Initialize LLM
 def initialize_llm(project_id,region,model_name,max_output_tokens,temperature,top_p,top_k):
    
     # Initialize VertexAI and set up the LLM
-    init_vertexai(project_id=project_id,region=region)
-    return set_up_llm(model_name=model_name,max_output_tokens=max_output_tokens,temperature=temperature,top_p=top_p,top_k=top_k)
-
-def init_vertexai(project_id,region):
-    # Initialize VertexAI with the proper settings
-    vertexai.init(project=project_id, location=region)
-
-def set_up_llm(model_name,max_output_tokens,temperature,top_p,top_k):
-    # Set up the VertexAI with the specified parameters
     return VertexAI(
+        project=project_id,
+        location=region,
         model_name=model_name,
         max_output_tokens=max_output_tokens,
         temperature=temperature,
