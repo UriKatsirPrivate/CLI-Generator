ac47b49afc7c01e82c18583432885ebc22941398
Uri Katsir <46541339+UriKatsirPrivate@users.noreply.github.com>
2023-09-11 22:50:58 +0300

    Added terraform creator

--- first.py
+++ first.py
@@ -14,7 +14,7 @@ temperature = st.sidebar.number_input('Enter temperature',min_value=0.0,max_valu
 top_p = st.sidebar.number_input('Enter top_p',min_value=0.0,max_value=1.0,step=0.1,value=0.8)
 top_k = st.sidebar.number_input('Enter top_k',min_value=1,max_value=40,step=1,value=40)
 
-tab1, tab2 = st.tabs(["CLI Generator", "Prompt Inspector"])
+tab1, tab2, tab3= st.tabs(["Generate CLI Commands", "Inspect My Prompt","Generate Terraform"])
 
 with tab1:
     def gcpCliCommandGenerator(user_input):
@@ -99,3 +99,35 @@ with tab2:
                 displaySafePrompt(safe_prompt)
         else:
             st.markdown("Please enter a prompt.")
+with tab3:
+    def terraformGenerator(description):
+        llm = initialize_llm(project_id,region,model_name,max_tokens,temperature,top_p,top_k)
+
+        system_template = """You are a terraform expert capable of generating Terraform files based on the user's description."""
+        system_message_prompt = SystemMessagePromptTemplate.from_template(system_template)
+        human_template = """The user's description is: '{description}'. Please generate the appropriate Terraform files based on this description."""
+        human_message_prompt = HumanMessagePromptTemplate.from_template(human_template)
+        chat_prompt = ChatPromptTemplate.from_messages(
+            [system_message_prompt, human_message_prompt]
+        )
+
+        chain = LLMChain(llm=llm, prompt=chat_prompt)
+        result = chain.run(description=description)
+        return result # returns string
+    def display_terraform_files(terraform_files):
+        if terraform_files:
+            st.markdown(f"### Generated Terraform Files: \n {terraform_files}")
+        else:
+            st.markdown("No Terraform files generated. Please provide a valid description.")        
+
+    description = st.text_area("Please enter the description of the desired architecture on GCP")
+    if st.button('Generate Terraform Files'):
+
+        if description:
+            terraform_files = terraformGenerator(description)
+            display_terraform_files(terraform_files)
+        else:
+            st.markdown("No description provided. Please enter a valid description.")
+    else:
+        terraform_files = ""
+        display_terraform_files(terraform_files)
