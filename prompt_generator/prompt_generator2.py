e4f76f93e5d3254c765302d2079864cbf5e68a19
Uri Katsir <46541339+UriKatsirPrivate@users.noreply.github.com>
2023-09-11 20:24:58 +0300

    Deleted one file

--- prompt_generator/prompt_generator2.py
+++ prompt_generator/prompt_generator2.py
@@ -6,10 +6,6 @@ from  initialization import initialize_llm
 
 initial_prompt = "Generate a workout schedule"
 
-# Initialize LLM
-# openai_api_key = "YOUR_OPENAI_API_KEY"
-# llm = ChatOpenAI(openai_api_key=openai_api_key, model="gpt-3.5-turbo-16k", temperature=0.4)
-
 llm = initialize_llm("landing-zone-demo-341118","us-central1","text-bison-32k","8192","0.1","0.8","40")
 
 
